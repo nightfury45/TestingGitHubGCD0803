e674a529b3085d76b6e8b2fe876653d00ad2e54e
nightfury45 <agentp207@gmail.com>
2021-06-10 07:55:36 +0700

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestingGitHubGCD0803.Startup))]
+namespace TestingGitHubGCD0803
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
