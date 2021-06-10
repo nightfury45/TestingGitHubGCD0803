1ba8bafa82ad617b6df5467b465c3b02d93bfd91
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-06-10 08:22:31 +0700

    Add VinhController

---
+++ Controllers/VinhController.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using TestingGitHubGCD0803.Models;
+
+namespace TestingGitHubGCD0803.Controllers
+{
+	public class VinhController : Controller
+	{
+		private ApplicationDbContext _context;
+		public VinhController()
+		{
+			_context = new ApplicationDbContext();
+		}
+		// GET: Vinh
+		public ActionResult Index()
+		{
+			var orders = _context.Orders.ToList();
+			return View(orders);
+		}
+	}
+}
