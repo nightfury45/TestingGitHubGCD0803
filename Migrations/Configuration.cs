99eb5edbbb7e449cf6d0679f0a97676b7588ff48
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-06-10 08:11:56 +0700

    Enable Migrations

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace TestingGitHubGCD0803.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TestingGitHubGCD0803.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(TestingGitHubGCD0803.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
